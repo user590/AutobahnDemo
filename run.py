25e3c2710e410e53eb887df55d0147d596053d92
evgeniy.shakhov <evgeniy.shakhov@rtlabs.ru>
2020-04-20 01:41:45 +0300

    add subscribe

--- run.py
+++ run.py
@@ -9,6 +9,7 @@ class AppSession(ApplicationSession):
     def onJoin(self, details):
         print("Connected!")
         yield self.register(self)
+        yield self.subscribe(self.got_public_message, "public")
 
     @wamp.register("remote_add")
     def remote_add(self, x, y):
@@ -19,6 +20,9 @@ class AppSession(ApplicationSession):
     def send_public(self, msg):
         self.publish("public", msg)
         return "OK"
+
+    def got_public_message(self, val):
+        print("Got public message: %s" % str(val))
 
 
 if __name__ == '__main__':
